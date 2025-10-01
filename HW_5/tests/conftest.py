711a872ec791c62fbe9ad41d9e66841d88030fc5
NickGor10 <63467263+NickGor10@users.noreply.github.com>
2025-10-01 11:32:06 +0000

    Delete HW_5 directory

--- HW_5/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# додаємо корінь проекту в sys.path, щоб імпорти типу from db.database працювали
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
